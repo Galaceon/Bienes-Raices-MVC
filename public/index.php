932d4a1642c228828cf3006c5e0fbe5fb3177c0c
Galaceon <antoniogarcialtr@gmail.com>
2025-07-10 06:51:56 +0200

    Router started

--- public/index.php
+++ public/index.php
@@ -1,1 +1,14 @@
-<p>Hola Mundo!!!</p>
+<?php
+
+require_once __DIR__ . '/../includes/app.php';
+
+use MVC\Router;
+
+$router = new Router();
+
+
+$router->get('/nosotros', 'funcion_nosotros');
+$router->get('/admin', 'funcion_admin');
+
+
+$router->comprobarRutas();
