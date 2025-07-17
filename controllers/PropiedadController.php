021c82f5771906ab2c897e98e7be2ac8fb4e574b
Galaceon <antoniogarcialtr@gmail.com>
2025-07-17 06:04:02 +0200

    save images in public folder and succes message CRUD added
    - propiedadController.php: added POST bool to create a new DB propertie and upload images
    - funciones.php: changed path of CARPETA_IMAGES and CARPETA_FOTOS
    - crear.php: I added enctype in form to upload images -- $errores array for error alerts added

--- controllers/PropiedadController.php
+++ controllers/PropiedadController.php
@@ -4,11 +4,16 @@ use MVC\Router;
 use Model\Propiedad;
 use Model\Vendedor;
 
+use Intervention\Image\Drivers\Gd\Driver;
+use Intervention\Image\ImageManager as Image;
+
 class PropiedadController {
     public static function index(Router $router) {
 
         $propiedades = Propiedad::all();
-        $resultado = null;
+        
+        // Muestra mensaje condicional
+        $resultado = $_GET['resultado'] ?? null;
 
         $router->render('propiedades/admin', [
             'propiedades' => $propiedades,
@@ -21,13 +26,47 @@ class PropiedadController {
         $propiedad = new Propiedad;
         $vendedores = Vendedor::all();
 
-        IF($_SERVER['REQUEST_METHOD'] === 'POST') {
-            
+        // Array con mensajes de errores
+        $errores = Propiedad::getErrores();
+
+        //Ejecutar el código despues de que el usuario envia el formulario
+        if($_SERVER['REQUEST_METHOD'] === 'POST') {
+            // Crea una nueva instancia
+            $propiedad = new Propiedad($_POST['propiedad']);
+
+            //Generar un nombre único para imagen
+            $nombreImagen = md5( uniqid(rand(), true) ) . ".jpg";
+
+            //Setear la imagen
+            // Realiza un resize a la imagen con intervention
+            if($_FILES['propiedad']['tmp_name']['imagen']) {
+                $manager = new Image(Driver::class);
+                $imagen = $manager->read($_FILES['propiedad']['tmp_name']['imagen'])->cover(800, 600);
+                $propiedad->setImagen($nombreImagen);
+            }
+
+            // Validar
+            $errores = $propiedad->validar();
+
+            // Revisar que el arreglo de errores este vacio para enviar la info a la db
+            if(empty($errores)) {
+                /** SUBIDA DE ARCHIVOS */
+                if(!is_dir(CARPETA_IMAGENES)){
+                    mkdir(CARPETA_IMAGENES);
+                }
+
+                // Guardar la imagen en el Servidor
+                $imagen->save(CARPETA_IMAGENES . $nombreImagen);
+
+
+                $propiedad->guardar();
+            }
         }
 
         $router->render('propiedades/crear', [
             'propiedad' => $propiedad,
-            'vendedores' => $vendedores
+            'vendedores' => $vendedores,
+            'errores' => $errores
         ]);
     }
 
--- includes/funciones.php
+++ includes/funciones.php
@@ -2,8 +2,8 @@
 
 define('TEMPLATES_URL', __DIR__ . '/templates'); //nombre de la constante y su ruta absoluta, gracias a __DIR__
 define('FUNCIONES_URL', __DIR__ . 'funciones.php'); // __DIR__ : Da la ruta absoluta de este archivo, C:\users\anton\Escritorio\bienesraices\includes\funciones.php
-define('CARPETA_IMAGENES', __DIR__ . '/../imagenes/');
-define('CARPETA_FOTOS', __DIR__ . '/../fotos/');
+define('CARPETA_IMAGENES', $_SERVER['DOCUMENT_ROOT'] . '/imagenes/');
+define('CARPETA_FOTOS', $_SERVER['DOCUMENT_ROOT'] . '/fotos/');
 
 function incluirTemplate( string $nombre, bool $inicio = false) {
     include  TEMPLATES_URL . "/$nombre.php";
