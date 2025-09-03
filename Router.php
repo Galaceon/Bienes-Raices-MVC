7177bf0c65e4de25bb9827a6869f3eb4232210de
Galaceon <antoniogarcialtr@gmail.com>
2025-09-03 05:58:33 +0200

    admin router protected

--- Router.php
+++ Router.php
@@ -15,7 +15,15 @@ class Router {
     }
 
     public function comprobarRutas() {
-        
+
+        session_start();
+        $auth = $_SESSION['login'] ?? null;
+
+        // Array de rutas protegidas
+        $rutas_protegidas = ['/admin', '/propiedades/crear', '/propiedades/actualizar', '/propiedades/eliminar', '/vendedores/crear', 
+        '/vendedores/actualizar', '/vendedores/eliminar', '/blogs/crear', '/blogs/actualizar', '/blogs/eliminar'];
+
+
         $urlActual = $_SERVER['PATH_INFO'] ?? '/'; //"/blogs/crear"
         $metodo = $_SERVER['REQUEST_METHOD']; // POST O GET
 
@@ -23,6 +31,11 @@ class Router {
             $fn = $this->rutasGET[$urlActual] ?? null;
         } else {
             $fn = $this->rutasPOST[$urlActual] ?? null;
+        }
+
+        // Proteger las rutas
+        if(in_array($urlActual, $rutas_protegidas) && !$auth) {
+            header('Location: /');
         }
 
         if($fn) {
